187b03c5517d31848913007c7d85e6255cde4020
DrawingProcess <sjchoi.dp@gmail.com>
2024-08-17 21:22:19 +0900

    Modify: space module - Change to Inheritance Structure

--- space/complex_grid_map.py
+++ space/complex_grid_map.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import random
 import math
 
+from space.grid_map import GridMap
+
 from route_planner.geometry import Pose
 
-class ComplexGridMap:
+class ComplexGridMap(GridMap):
     def __init__(self, lot_width=82, lot_height=63):
         self.lot_width = lot_width
         self.lot_height = lot_height
@@ -54,75 +56,6 @@ class ComplexGridMap:
             [(start_x + width - 1, start_y), (start_x + width - 1, start_y + height - 1)],
             [(start_x, start_y + height - 1), (start_x + width - 1, start_y + height - 1)],
         ])
-
-    def is_obstacle(self, x, y):
-        return self.grid[y][x] == 1
-
-    def is_valid_position(self, x, y):
-        # 주어진 좌표가 장애물이 아니고 맵 범위 내에 있는지 확인
-        return 0 <= x < self.lot_width and 0 <= y < self.lot_height and not self.is_obstacle(x, y)
-
-    def get_random_valid_position(self):
-        while True:
-            x = random.randint(0, self.lot_width - 1)
-            y = random.randint(0, self.lot_height - 1)
-            if self.is_valid_position(x, y):
-                return Pose(x, y, math.radians(0))
-            
-    def get_random_valid_start_position(self):
-        while True:
-            x = random.randint(0, self.lot_width // 4)
-            y = random.randint(0, self.lot_height // 4)
-            if self.is_valid_position(x, y):
-                return Pose(x, y, math.radians(0))
-            
-    def get_random_valid_goal_position(self):
-        while True:
-            x = random.randint(self.lot_width * 3 // 4, self.lot_width - 1)
-            y = random.randint(self.lot_height * 3 // 4, self.lot_height - 1)
-            if self.is_valid_position(x, y):
-                return Pose(x, y, math.radians(90))
-
-    def is_not_crossed_obstacle(self, previous_node, current_node):
-        is_cross_line = any(
-            [
-                self.intersect(obstacle_line, [previous_node, current_node])
-                for obstacle_line in self.obstacle_lines
-            ]
-        )
-        return (
-            current_node not in set(self.obstacles)
-            and 0 < current_node[0] < self.lot_width
-            and 0 < current_node[1] < self.lot_height
-            and not is_cross_line
-        )
-
-    def intersect(self, line1, line2):
-        A = line1[0]
-        B = line1[1]
-        C = line2[0]
-        D = line2[1]
-        return self.ccw(A, C, D) != self.ccw(B, C, D) and self.ccw(A, B, C) != self.ccw(A, B, D)
-
-    def ccw(self, A, B, C):
-        return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
-
-    def plot_map(self, path=None):
-        obstacle_x = [x for x, y in self.obstacles]
-        obstacle_y = [y for x, y in self.obstacles]
-        plt.plot(obstacle_x, obstacle_y, ".k")  # 장애물은 검은색 사각형으로 표시
-        # plt.plot(obstacle_x, obstacle_y, "sk")  # 장애물은 검은색 사각형으로 표시
-
-        # Plot the obstacle lines
-        for line in self.obstacle_lines:
-            x_values = [line[0][0], line[1][0]]
-            y_values = [line[0][1], line[1][1]]
-            plt.plot(x_values, y_values, "k-")  # 장애물 라인은 검은 실선으로 표시
-
-        if path:
-            path_x = [x for x, y in path]
-            path_y = [y for x, y in path]
-            plt.plot(path_x, path_y, "-or")  # 경로는 빨간색 원으로 연결된 선으로 표시
 
 if __name__ == "__main__":
     # 맵 크기를 지정하여 GridMap 생성 (예: 100x75)
--- space/parking_lot.py
+++ space/parking_lot.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 import random
 import math
 
+from space.grid_map import GridMap
+
 from route_planner.geometry import Pose
 
-class ParkingLot:
+class ParkingLot(GridMap):
     def __init__(self, lot_width=100, lot_height=80, space_width=6, space_height=11):
+        super().__init__(lot_width, lot_height)
         # 주차장 크기 및 주차 공간 크기 설정
         self.lot_width = lot_width
         self.lot_height = lot_height
@@ -18,21 +21,6 @@ class ParkingLot:
         self.create_outer_walls()
         self.create_horizontal_lines()
         self.create_vertical_lines()
-
-    def create_outer_walls(self):
-        # 주차장의 외벽 생성
-        for x in range(self.lot_width + 1):
-            self.obstacles.append((x, 0))
-            self.obstacles.append((x, self.lot_height))
-        for y in range(1, self.lot_height):
-            self.obstacles.append((0, y))
-            self.obstacles.append((self.lot_width, y))
-        self.obstacle_lines.extend([
-            [(0, 0), (0, self.lot_height)],
-            [(0, 0), (self.lot_width, 0)],
-            [(self.lot_width, 0), (self.lot_width, self.lot_height)],
-            [(0, self.lot_height), (self.lot_width, self.lot_height)],
-        ])
 
     def create_horizontal_lines(self):
         # 주차장 내부의 가로선을 생성
@@ -52,76 +40,6 @@ class ParkingLot:
                 for h in range(self.space_height):
                     self.obstacles.append((11 + x * self.space_width, y + h - self.space_height//2))
                 self.obstacle_lines.append([(11 + x * self.space_width, y - self.space_height//2), (11 + x * self.space_width, y + self.space_height//2)])
-
-    def get_grid_index(self, x, y):
-        return x + y * self.lot_width
-
-    def is_valid_position(self, x, y):
-        # 주어진 좌표가 장애물이 아니고 맵 범위 내에 있는지 확인
-        return 0 <= x < self.lot_width and self.space_height <= y < self.lot_height - self.space_height
-
-    def get_random_valid_position(self):
-        while True:
-            x = random.randint(0, self.lot_width - 1)
-            y = random.randint(self.space_height, self.lot_height - 1 - self.space_height)
-            if self.is_valid_position(x, y):
-                return Pose(x, y, math.radians(0))
-            
-    def get_random_valid_start_position(self):
-        while True:
-            x = random.randint(0, self.lot_width // 4)
-            y = random.randint(self.space_height, self.lot_height // 4)
-            if self.is_valid_position(x, y):
-                return Pose(x, y, math.radians(0))
-            
-    def get_random_valid_goal_position(self):
-        while True:
-            x = random.randint(self.lot_width * 3 // 4, self.lot_width - 1)
-            y = random.randint(self.lot_height * 3 // 4, self.lot_height - 1 - self.space_height)
-            if self.is_valid_position(x, y):
-                return Pose(x, y, math.radians(90))
-
-    def is_not_crossed_obstacle(self, previous_node, current_node):
-        is_cross_line = any(
-            [
-                self.intersect(obstacle_line, [previous_node, current_node])
-                for obstacle_line in self.obstacle_lines
-            ]
-        )
-        return (
-                current_node not in set(self.obstacles)
-                and 0 < current_node[0] < self.lot_width
-                and 0 < current_node[1] < self.lot_height
-                and not is_cross_line
-        )
-
-    def intersect(self, line1, line2):
-        A = line1[0]
-        B = line1[1]
-        C = line2[0]
-        D = line2[1]
-        return self.ccw(A, C, D) != self.ccw(B, C, D) and self.ccw(A, B, C) != self.ccw(A, B, D)
-
-    def ccw(self, A, B, C):
-        return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
-    
-
-    def plot_map(self, path=None):
-        obstacle_x = [x for x, y in self.obstacles]
-        obstacle_y = [y for x, y in self.obstacles]
-        plt.plot(obstacle_x, obstacle_y, ".k")  # 장애물은 검은색 사각형으로 표시
-        # plt.plot(obstacle_x, obstacle_y, "sk")  # 장애물은 검은색 사각형으로 표시
-
-        # Plot the obstacle lines
-        for line in self.obstacle_lines:
-            x_values = [line[0][0], line[1][0]]
-            y_values = [line[0][1], line[1][1]]
-            plt.plot(x_values, y_values, "k-")  # 장애물 라인은 검은 실선으로 표시
-
-        if path:
-            path_x = [x for x, y in path]
-            path_y = [y for x, y in path]
-            plt.plot(path_x, path_y, "-or")  # 경로는 빨간색 원으로 연결된 선으로 표시
 
 if __name__ == "__main__":
     # 주차장의 크기를 설정 (예: lot_width=120, lot_height=100 등으로 설정 가능)
