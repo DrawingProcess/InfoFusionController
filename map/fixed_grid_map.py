41467efe585879dc796c4504129f6de68ab2cb2c
DrawingProcess <sjchoi.dp@gmail.com>
2024-10-13 22:24:43 +0900

    Add: change of dynamic obstacle color

--- map/fixed_grid_map.py
+++ map/fixed_grid_map.py
@@ -2,12 +2,19 @@ import matplotlib.pyplot as plt
 import math
 import argparse
 import json
+import random
+import numpy as np
+
+from utils import transform_trajectory_with_angles
 
 from map.grid_map import GridMap
 
 class FixedGridMap(GridMap):
     def __init__(self, width=20, height=20, obstacles=None):
         super().__init__(width=width, height=height)
+
+        self.obstacles_dynamic = []
+        self.obstacle_lines_dynamic = []
 
         if obstacles:
             self.add_config_obstacles(obstacles)
@@ -31,19 +38,27 @@ class FixedGridMap(GridMap):
             else:
                 print(f"Unknown obstacle type: {obstacle['type']}")
 
-    def add_fixed_rectangle(self, min_x, min_y, max_x, max_y):
+    def add_fixed_rectangle(self, min_x, min_y, max_x, max_y, is_dynamic=False):
         # Add obstacle coordinates
         fixed_obstacles = [
             [(x, y) for x in range(min_x, max_x + 1) for y in range(min_y, max_y + 1)]
         ]
+        valid_points = []
         
         # Add obstacle positions to grid and obstacles list
         for group in fixed_obstacles:
             for x, y in group:
-                if 0 <= x < self.width and 0 <= y < self.height:
-                    self.obstacles.append((x, y))
+                if self.is_valid_position(x, y):
+                    valid_points.append((x, y))
                     self.grid[y][x] = 1
+                    if is_dynamic:
+                        self.obstacles_dynamic.append((x, y))
+                    else:
+                        self.obstacles.append((x, y))
         
+        if not valid_points:
+            return  # 유효한 좌표가 없으면 종료
+
         # Add obstacle boundary lines
         box_lines = [
             ((min_x, min_y), (max_x, min_y)),  # Top line
@@ -51,7 +66,10 @@ class FixedGridMap(GridMap):
             ((min_x, min_y), (min_x, max_y)),  # Left line
             ((max_x, min_y), (max_x, max_y)),  # Right line
         ]
-        self.obstacle_lines.extend(box_lines)
+        if is_dynamic:
+            self.obstacle_lines_dynamic.extend(box_lines)
+        else:
+            self.obstacle_lines.extend(box_lines)
         
     def add_fixed_circle(self, center_x, center_y, radius):
         # Ensure the list exists to store circular obstacles
@@ -81,6 +99,61 @@ class FixedGridMap(GridMap):
         # Connect the points to form a circle outline
         for i in range(len(points)):
             self.obstacle_lines.append((points[i], points[(i + 1) % len(points)]))
+
+    # map_instance.create_random_obstacles_in_path(ref_trajectory, n=3, box_size=(5, 5))
+    def create_random_obstacles_in_path(self, ref_trajectory, n=3, box_size=(5, 5)):
+        ref_trajectory = transform_trajectory_with_angles(ref_trajectory)
+        for _ in range(n):
+            idx = random.randint(len(ref_trajectory)//4, len(ref_trajectory)*3//4 - 1)
+            x, y, _, _ = ref_trajectory[idx]
+            if self.is_valid_position(x, y):
+                pos = random.randint(0, box_size[0] // 2)
+                self.add_fixed_rectangle(int(x) - pos, int(y) - pos, int(x) - pos + box_size[0], int(y) - pos + box_size[1], is_dynamic=True)
+
+
+    def plot_map(self, title, map_path=None, path=None):
+        if map_path:
+            # Read and display background image
+            bg_image = plt.imread(map_path)
+            plt.imshow(bg_image, extent=[0, self.width, 0, self.height], origin='lower', cmap=plt.cm.gray)
+        
+        obstacle_x = [x for x, y in self.obstacles]
+        obstacle_y = [y for x, y in self.obstacles]
+        plt.plot(obstacle_x, obstacle_y, ".k")  # 장애물은 검은색 점으로 표시
+
+        # 사각형 장애물의 경계선 표시
+        for line in self.obstacle_lines:
+            x_values = [line[0][0], line[1][0]]
+            y_values = [line[0][1], line[1][1]]
+            plt.plot(x_values, y_values, "k-")  # 장애물 라인은 검은 실선으로 표시
+        
+        if self.obstacles_dynamic is not None:
+            obstacle_x = [x for x, y in self.obstacles_dynamic]
+            obstacle_y = [y for x, y in self.obstacles_dynamic]
+            plt.plot(obstacle_x, obstacle_y, ".g")  # 동적 장애물은 빨간색 점으로 표시
+
+            for line in self.obstacle_lines_dynamic:
+                x_values = [line[0][0], line[1][0]]
+                y_values = [line[0][1], line[1][1]]
+                plt.plot(x_values, y_values, "g-")
+
+        # 원형 장애물 표시
+        for center_x, center_y, radius in self.circular_obstacles:
+            circle = plt.Circle((center_x, center_y), radius, color='black', fill=False)
+            plt.gca().add_patch(circle)
+
+        if path:
+            path_x = [x for x, y in path]
+            path_y = [y for x, y in path]
+            plt.plot(path_x, path_y, "-or")  # 경로는 빨간색 원으로 연결된 선으로 표시
+
+        plt.xlim(-1, self.width + 1)
+        plt.ylim(-1, self.height + 1)
+        plt.title(title)
+        plt.xlabel("X [m]")
+        plt.ylabel("Y [m]")
+        plt.grid(True)
+        plt.axis("equal")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate Fixed Grid Map with obstacles.')
